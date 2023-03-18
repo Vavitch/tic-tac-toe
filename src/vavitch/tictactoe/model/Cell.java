df35d783577f52b8c6f6faa75ddb2a2d6978e42c
admin <vavitch@yahoo.com>
2023-03-18 12:09:18 +0200

    add model classes

--- src/vavitch/tictactoe/model/Cell.java
+++ src/vavitch/tictactoe/model/Cell.java
@@ -21,19 +21,22 @@ package vavitch.tictactoe.model;
  * @author vavitch
  * @link vavitch@yahoo.com
  */
-public class GameTable {
+public class Cell {
 
-    private final char[][] table = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private final int row;
 
-    public boolean isEmpty(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()] == ' ';
+    private final int col;
+
+    public Cell(final int row, final int col) {
+        this.row = row;
+        this.col = col;
     }
 
-    public char getSign(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()];
+    public int getRow() {
+        return row;
     }
 
-    public void setSign(final Cell cell, final char sign) {
-        table[cell.getRow()][cell.getCol()] = sign;
+    public int getCol() {
+        return col;
     }
 }
