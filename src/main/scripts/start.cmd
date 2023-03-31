4d9c2a726da7c2937ae1f3fdbb2b4163b5e63d08
admin <vavitch@yahoo.com>
2023-03-31 12:27:01 +0300

    add scripts to start and create assembly configuration

---
+++ src/main/scripts/start.cmd
@@ -0,0 +1,5 @@
+@rem game tic-tac-toe in console-mode
+@echo off
+java  -jar tic-tac-toe.jar
+@echo press any key
+pause
---
+++ src/main/scripts/start.sh
@@ -0,0 +1,6 @@
+#!/user/bin/env sh
+
+# game tic-tac-toe in console-mode
+
+
+java -jar tic-tac-toe.jar
